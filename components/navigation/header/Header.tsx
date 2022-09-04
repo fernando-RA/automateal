b76272dd59ab31a87b80fea857b8768e686dae5e
Fernando <fraguilar@pm.me>
2022-09-03 22:42:06 -0300

    feat: adding layout component

---
+++ components/navigation/header/Header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
+
+const Header: React.FC<IHeader> = ({ className, ...headerProps }) => {
+  return (
+    <header
+      {...headerProps}
+      className={`w-full flex flex-row justify-between ${className}`}
+    >
+      <div className="space-x-5 m-5">
+        <Link href="/">
+          <a className="hover:underline">About</a>
+        </Link>
+        <Link href="/">
+          <a className="hover:underline">Images</a>
+        </Link>
+      </div>
+      <div className="space-x-5 m-5">
+        <Link href="/">
+          <a className="hover:underline hidden sm:inline">Gmail</a>
+        </Link>
+        <Link href="/">
+          <a className="hover:underline hidden sm:inline">Store</a>
+        </Link>
+        <button className="border-1 p-2 px-4 sm:px-6 bg-blue-500 rounded text-white">
+          Sign In
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
-import styles from '../styles/Home.module.css';
+import Search from '../components/widgets/search/search';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className="bg-gradient-to-r from-blue-300 to-teal-500">
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <CatCard {...mockCatCardProps.base} />
+    <section>
+      logo
+      <Search />
+      language toggle
     </section>
   );
 };
@@ -19,10 +15,5 @@ const Home: NextPageWithLayout = () => {
 export default Home;
 
 Home.getLayout = (page) => {
-  return (
-    <PrimaryLayout>
-      <SidebarLayout />
-      {page}
-    </PrimaryLayout>
-  );
+  return <PrimaryLayout>{page}</PrimaryLayout>;
 };
