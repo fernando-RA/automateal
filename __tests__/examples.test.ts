2b5bc418bdd79a879906006319a9c79249fe4332
Fernando <fraguilar@pm.me>
2022-11-27 13:05:57 -0300

    build: fixing jest

---
+++ __tests__/examples.test.ts
@@ -0,0 +1,9 @@
+import { createMocks } from 'node-mocks-http';
+import examples from '../src/pages/api/examples.api'
+
+describe('/api/examples', () => {
+  test('bs test', async () => {
+    // TODO -> https://www.prisma.io/docs/guides/testing/unit-testing
+    expect(true).toBeTruthy()
+  });
+});
--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     collectCoverage: true,
     // on node 14.x coverage provider v8 offers good speed and more or less good report
     coverageProvider: 'v8',
